f937e909de901dcdb4b0546f77af11c41652628a
Ceoriverside26 <Ceo@riversideluxeretreats.com>
2026-04-27 14:01:18 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
 const BOOKING_CONFIG = {
-  squareBookingUrl: "PASTE_YOUR_SQUARE_BOOKING_LINK_HERE",
-  phone: "(555) 123-4567",
-  email: "info@homecomfortcleaners.com",
-  businessName: "Home Comfort Cleaners LLC"
+  squareLink: "PASTE_YOUR_SQUARE_BOOKING_LINK_HERE"
 };
