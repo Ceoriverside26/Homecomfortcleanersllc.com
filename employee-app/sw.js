d4440efd4e503b1b7a23f4f4c8b6fdb6daf4d975
Ceoriverside26 <Ceo@riversideluxeretreats.com>
2026-09-18 18:15:47 -0400

    Refresh cache for payroll portal

--- employee-app/sw.js
+++ employee-app/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hcc-employee-v9';
-const ASSETS=['./','./index.html','./management.html','./jobs-management.html','./team-lead.html','./onboarding.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+const CACHE='hcc-employee-v10';
+const ASSETS=['./','./index.html','./management.html','./jobs-management.html','./team-lead.html','./onboarding.html','./payroll.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
